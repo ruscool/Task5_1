c6ef9f2295011266c509e6eb0290956a1ebb7058
RuslanR <ruscool79@gmail.com>
2024-04-21 19:48:18 +0300

    add Task5_1

---
+++ src/Box.java
@@ -0,0 +1,16 @@
+public class Box<T> {
+    private T item;
+
+    public void addItem(T newItem) {
+        this.item = newItem;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public boolean isEmpty() {
+        return item == null;
+    }
+
+}
---
+++ src/Product.java
@@ -0,0 +1,27 @@
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
+class Fruit extends Product {
+    public Fruit(String name, double price) {
+        super(name, price);
+    }
+}
+
+class Vegetable extends Product {
+    public Vegetable(String name, double price) {
+        super(name, price);
+    }
+}
+
+
